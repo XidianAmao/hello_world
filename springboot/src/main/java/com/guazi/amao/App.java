08b949bfacabd95bf72962267f4430f9b32ab2d4
guangtong.feng <guangtong.feng@qunar.com>
2019-08-02 23:40:40 +0800

    spring boot init

---
+++ springboot/src/main/java/com/guazi/amao/App.java
@@ -0,0 +1,19 @@
+package com.guazi.amao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class App {
+    @RequestMapping("/")
+    public String index(){
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
