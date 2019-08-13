fddafff522feb97f3103eefd63a2eedbd2984448
fengguangtong <fengguangtong@guazi.com>
2019-08-13 21:21:33 +0800

    add template method

---
+++ springboot/src/main/java/com/amao/guava/design/AbstractClazz.java
@@ -0,0 +1,21 @@
+package com.amao.guava.design;
+
+import com.amao.guava.AppService;
+
+import javax.annotation.Resource;
+
+/**
+ * https://www.cnblogs.com/wangkaihua/p/9123171.html
+ * https://blog.csdn.net/m0_37542889/article/details/83014385
+ * @author fengguangtong@guazi.com
+ * @version 1.0
+ * @date 2019-08-13 21:17
+ */
+public abstract class AbstractClazz {
+    @Resource
+    private AppService appService;
+
+    public void execute(){
+        appService.say();
+    }
+}
---
+++ springboot/src/main/java/com/amao/guava/design/ConcreteClazz2.java
@@ -0,0 +1,9 @@
+package com.amao.guava.design;
+
+/**
+ * @author fengguangtong@guazi.com
+ * @version 1.0
+ * @date 2019-08-13 21:20
+ */
+public class ConcreteClazz2 extends AbstractClazz {
+}
